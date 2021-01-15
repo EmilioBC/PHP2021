291c903e1a21de46d4c8e4fedc3422988276d7f7
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-15 08:01:08 +0000

    Area

--- Section3/index.php
+++ Section3/index.php
@@ -1,3 +1,57 @@
 <?php
 
-echo ("Section 3: Basic Operators");
+// echo ("Section 3: Basic Operators");
+
+// echo "<br>";
+
+// $number1 = 20;
+
+// $number2 = 5;
+
+// $answer = $number1 / $number2;
+
+// echo $answer;
+
+
+//////////////////// EXPONENT RAISE TO  THE POWER ///////////////////
+
+// $base = 2;
+
+// $power = 3;
+
+// $answer = pow($base, $power);
+
+// echo $answer;
+
+
+///////////////// BACK TO SCHOOL CHALLENGE /////////////////////
+
+/// Area of rectangle
+$length = 10;
+
+$width = 5;
+
+$rectangle = $length * $width;
+
+echo "Area of rectangle is " .  $rectangle;
+
+echo "<br>";
+
+/// Area of Triangle///////
+$base = 12;
+
+$height = 3;
+
+$triangle = ($base * $height) / 2;
+
+echo "Area of a Triangle is " . $triangle;
+
+echo "<br>";
+
+//// Area of Circle//////////
+
+$pie = 3.15;
+
+$radBase = 5;
+$radPow = 2;
+$radius = pow($radBase, $radPow);
