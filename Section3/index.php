6abb75c53634877c085eee36e0a86971cee9e9c4
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-16 05:02:06 +0000

    endOfSection3

--- Section3/index.php
+++ Section3/index.php
@@ -5,53 +5,55 @@
 // echo "<br>";
 
 // $number1 = 20;
-
 // $number2 = 5;
-
 // $answer = $number1 / $number2;
-
 // echo $answer;
 
 
 //////////////////// EXPONENT RAISE TO  THE POWER ///////////////////
 
 // $base = 2;
-
 // $power = 3;
-
 // $answer = pow($base, $power);
-
 // echo $answer;
 
 
 ///////////////// BACK TO SCHOOL CHALLENGE /////////////////////
 
 /// Area of rectangle
-$length = 10;
+// $length = 10;
+// $width = 5;
+// $rectangle = $length * $width;
+// echo "Area of rectangle is " .  $rectangle;
 
-$width = 5;
-
-$rectangle = $length * $width;
-
-echo "Area of rectangle is " .  $rectangle;
-
-echo "<br>";
+// echo "<br>";
 
 /// Area of Triangle///////
-$base = 12;
+// $base = 12;
+// $height = 3;
+// $triangle = ($base * $height) / 2;
+// echo "Area of a Triangle is " . $triangle;
 
-$height = 3;
-
-$triangle = ($base * $height) / 2;
-
-echo "Area of a Triangle is " . $triangle;
-
-echo "<br>";
+// echo "<br>";
 
 //// Area of Circle//////////
 
-$pie = 3.15;
+// $pie = 3.15;
 
-$radBase = 5;
-$radPow = 2;
-$radius = pow($radBase, $radPow);
+// $radBase = 5;
+// $radPow = 2;
+// $radius = pow($radBase, $radPow);
+// echo "Radius of 5 raise to the power of 2 is: " . $radius;
+// echo "<br>";
+// $areaOfCircle = ($pie * $radius) * 25;
+// echo "Area of a cricle is: " . $areaOfCircle . " sq in";
+
+
+////////// Area of Trapezium /////////////
+
+// $sideA = 10;
+// $sideB = 8;
+// $height = 4;
+// echo "<br>";
+// $areaTrapezium = ((($sideA + $sideB) / 2) * 8);
+// echo "The area of Trapezium is: " . $areaTrapezium;
