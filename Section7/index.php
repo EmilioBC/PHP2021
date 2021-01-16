00ca8c4ebe2f5d9705c9f7bafec539e9444589f9
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-16 09:32:01 +0000

    ChallIwin

---
+++ Section7/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//////// PHP Built in Function //////////
+
+/////// Using PHP.net ////////////////
+
+// $randomNumber = rand(1, 100);
+
+// echo $randomNumber;
+
+
+//////////// I WIN CHALLENGE /////////////////
+
+
+
+// $heads = 0;
+// $tails = 0;
+
+// for ($i = 1; $i <= 100; $i++) {
+//   $randomNumber = rand(1, 2);
+//   if ($randomNumber == 1) {
+//     $heads++;
+//   } else if ($randomNumber == 2) {
+//     $tails++;
+//   }
+// }
+// echo "There were " . $heads . " heads and " . $tails . " tails";
