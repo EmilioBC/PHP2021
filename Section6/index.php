68b747138b4f708ce786ded8f0a8490696b1c0cd
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-16 06:34:08 +0000

    fizzBuzz

--- Section6/index.php
+++ Section6/index.php
@@ -30,3 +30,19 @@
 //   echo $j * 6 . "<br>";
 //   $j++;
 // }
+
+
+///////////////// FIZZ BUZZ CHALLENGE /////////
+
+for ($i = 1; $i <= 100; $i++) {
+
+  if ($i % 3 == 0  && $i % 5 == 0) {
+    echo "FIZZBUZZ <br>";
+  } else if ($i % 3 == 0) {
+    echo "FIZZ <br>";
+  } else if ($i % 5 == 0) {
+    echo "BUZZ <br>";
+  } else {
+    echo $i . "<br>";
+  }
+}
