75a6c8e4674009f2d8f207c28ee31919631e39c5
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-16 06:23:25 +0000

    Section6

---
+++ Section6/index.php
@@ -0,0 +1,32 @@
+<?php
+
+////////// FOR LOOPS ////////////
+
+// for ($i = 2; $i <= 20; $i += 2) {
+//   echo $i . "<br>";
+// }
+
+////////// WHILE LOOPS /////////////
+
+// $i = 1;
+
+// while ($i <= 10) {
+//   echo $i . "<br>";
+//   $i++;
+// }
+
+////////// CHALLENGE MULTIPLE BY 6. //////////
+// echo "For Loop Multiply by 6. <br> ";
+// for ($i = 1; $i <= 10; $i++) {
+//   echo $i * 6 . "<br>";
+// }
+
+// echo "<br>";
+// echo "While Loop Multiply by 6. <br>";
+
+// $j = 1;
+
+// while ($j <= 10) {
+//   echo $j * 6 . "<br>";
+//   $j++;
+// }
