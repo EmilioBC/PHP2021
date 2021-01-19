9eb5c70f85e04d6ea1eed7b75ac90797cbacc37b
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-19 00:26:41 +0000

    temperature

--- Section8Forms/html-form-processor.php
+++ Section8Forms/html-form-processor.php
@@ -1,4 +1,28 @@
 <?php
 
-echo "<pre>";
-print_r($_POST);
+// echo "<pre>";
+// print_r($_POST);
+
+if (isset($_POST)) {
+
+  $surname = $_POST['surname'];
+
+  $gender = $_POST['gender'];
+
+  echo "Your surname is " . $surname . " and your gender is " . $gender;
+
+  echo "<br>";
+  echo "<br>";
+}
+
+
+
+?>
+
+
+<form action="index.php" method="POST">
+  <input type="text" name="surname" value="Jay">
+  Male<input type="radio" name="gender" value="male">
+  Female<input type="radio" name="gender" value="female">
+  <input type="submit" name="submit" value="submit">
+</form>
--- Section8Forms/index.php
+++ Section8Forms/index.php
@@ -1,5 +1,34 @@
 <?php
 
+if (isset($_POST)) {
 
-echo "<pre>";
-print_r($_POST);
+  $temperature = $_POST['temperature'];
+  $degrees = $_POST['degrees'];
+
+
+  if ($degrees == "C") {
+    echo "The Temperature is " . $temperature . $degrees;
+    echo "<br>";
+    echo "The conversion to Farenheit is " . ($temperature - 32) * 5 / 9 . " Farenheit";
+  }
+
+  if ($degrees == "F") {
+    $farToCel = $temperature * 9 / 5 + 32;
+    echo "The Temperature is " . $temperature . $degrees;
+    echo "<br>";
+    echo "The conversion to Celsius is " . $farToCel . " Celsius";
+  }
+
+  echo "<br>";
+  echo "<br>";
+}
+
+
+?>
+
+<form action="index.php" method="POST">
+  <input type="text" name="temperature" value="">
+  Celsius<input type="radio" name="degrees" value="C">
+  Farenheit<input type="radio" name="degrees" value="F">
+  <input type="submit" name="submit" value="submit">
+</form>
