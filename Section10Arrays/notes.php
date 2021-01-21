7ba04e1a906e797cef0ae698acfb9dca37edb330
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-21 01:28:24 +0000

    isbnEND

--- Section10Arrays/notes.php
+++ Section10Arrays/notes.php
@@ -41,3 +41,57 @@ $letters = str_split($word, 4);
 echo "<pre>";
 
 print_r($letters);
+
+
+
+
+echo "______________ISBN CHECKER ____________________";
+
+if (isset($_POST["submit"])) {
+
+  $isbn = $_POST["isbn"];
+
+  if (mb_strlen($isbn) === 10) {
+    echo "Enter";
+
+    $splitNumbers = str_split($isbn);
+    echo "<pre>";
+    print_r($splitNumbers);
+
+    $addNumbers = 0;
+
+    for ($i = 0; $i <= 9; $i++) {
+      $splitTimes = $splitNumbers[$i] * ($i + 1);
+      $addNumbers += $splitTimes;
+      $modulus = $addNumbers / 11;
+
+      echo "<pre>";
+      print_r($splitTimes);
+      echo "<br>";
+      echo "<br>";
+    }
+    echo ("Add numbers is: " . $addNumbers);
+    echo "<br>";
+    echo "<br>";
+
+    if (is_int($modulus) === true) {
+      echo "This is a VALID ISBN number";
+    } else {
+      echo "This is NOT a VALID ISBN number";
+    }
+
+    echo "<br>";
+  } else {
+    echo "Must be 10 digits";
+  }
+}
+
+echo "<br>";
+echo "<br>";
+
+?>
+
+<form action="" method="POST">
+  ISBN number-<input type="number" name="isbn" value="isbn">
+  <input type="submit" name="submit" value="submit">
+</form>
