3356dd1d3f5e75b05e3dbea669e6888eb05ced7c
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-26 00:59:00 +0000

    CHALLENGEcapital

--- Section11AssociativeArr/index.php
+++ Section11AssociativeArr/index.php
@@ -1,20 +1,17 @@
 <?php
 echo "<pre>";
 
-$cities = ["London", "New York", "Paris", "Amsterdam", "Berlin"];
-// print_r($cities);
-foreach ($cities as $city) {
-  echo $city;
-  echo "<br>";
-}
+$cities = [
+  "Italy" => "Rome", "Luxembourg" => "Luxembourg", "Belgium" => "Brussels",
+  "Denmark" => "Copenhagen", "Finland" => "Helsinki", "France" => "Paris",
+  "Slovakia" => "Bratislava", "Slovenia" => "Ljubljana", "Germany" => "Berlin",
+  "Greece" => "Athens", "Ireland" => "Dublin", "Netherlands" => "Amsterdam",
+  "Portugal" => "Lisbon", "Spain" => "Madrid", "Sweden" => "Stockholm", "United Kingdom" => "London", "Cyprus" => "Nicosia", "Lithuania" => "Vilnius", "Czech Republic" => "Prague", "Estonia" => "Tallin", "Hungary" => "Budapest",
+  "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna",
+  "Poland" => "Warsaw"
+];
 
-echo "<br>";
-echo "<br>";
-
-$students = ["Jay" => 37, "Beth" => 34, "RJ" => 6, "LJ" => 5];
-// print_r($students);
-
-foreach ($students as $student => $age) {
-  echo $student . ' is ' . $age . ' years old';
+foreach ($cities as $country => $city) {
+  echo "The capital city of " . $country . " is " . $city;
   echo "<br>";
 }
--- Section11AssociativeArr/notes.php
+++ Section11AssociativeArr/notes.php
@@ -30,3 +30,24 @@ foreach ($students as $student => $age) {
   echo $student . ' is ' . $age . ' years old';
   echo "<br>";
 }
+
+
+
+echo " -------------- CHALLENGE Capital City of Countries -----------------------";
+
+echo "<pre>";
+
+$cities = [
+  "Italy" => "Rome", "Luxembourg" => "Luxembourg", "Belgium" => "Brussels",
+  "Denmark" => "Copenhagen", "Finland" => "Helsinki", "France" => "Paris",
+  "Slovakia" => "Bratislava", "Slovenia" => "Ljubljana", "Germany" => "Berlin",
+  "Greece" => "Athens", "Ireland" => "Dublin", "Netherlands" => "Amsterdam",
+  "Portugal" => "Lisbon", "Spain" => "Madrid", "Sweden" => "Stockholm", "United Kingdom" => "London", "Cyprus" => "Nicosia", "Lithuania" => "Vilnius", "Czech Republic" => "Prague", "Estonia" => "Tallin", "Hungary" => "Budapest",
+  "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna",
+  "Poland" => "Warsaw"
+];
+
+foreach ($cities as $country => $city) {
+  echo "The capital city of " . $country . " is " . $city;
+  echo "<br>";
+}
