e02faa54d6af3a4b91b380ea07d6b6c284900e15
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-26 00:52:07 +0000

    LoopThruAssociative

---
+++ Section11AssociativeArr/notes.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+$students = ["Jay" => 37, "Beth" => 34, "RJ" => 6, "LJ" => 5];
+
+echo "<pre>";
+print_r($students);
+
+
+echo " -------------------- Loop through Associatuve -----------";
+
+
+echo "<pre>";
+
+$cities = ["London", "New York", "Paris", "Amsterdam", "Berlin"];
+// print_r($cities);
+foreach ($cities as $city) {
+  echo $city;
+  echo "<br>";
+}
+
+echo "<br>";
+echo "<br>";
+
+$students = ["Jay" => 37, "Beth" => 34, "RJ" => 6, "LJ" => 5];
+// print_r($students);
+
+foreach ($students as $student => $age) {
+  echo $student . ' is ' . $age . ' years old';
+  echo "<br>";
+}
