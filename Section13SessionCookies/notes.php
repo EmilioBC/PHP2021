5159fa2f64a299fef431b3936241cbeae03ed14e
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-26 05:59:24 +0000

    Cookies

---
+++ Section13SessionCookies/notes.php
@@ -0,0 +1,12 @@
+/*
+In order to make a cookie.
+
+- Dont make Index.php
+- Open the setcookie in the directory.
+- Go to chrome://settings/coookies
+- See all cookies
+- Search for Localhost then then the cookie element e.g. "fruit"
+- If new cookie will not show up, delete the current and get out of cookie page and come back.
+
+
+*/
