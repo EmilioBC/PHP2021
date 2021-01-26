a16b19cfd6a8bcba046a875179ea0cbd40e2f4a2
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-26 00:17:46 +0000

    CHALLENGEcardValid

--- Section10Arrays/index.php
+++ Section10Arrays/index.php
@@ -1,12 +1,43 @@
 <?php
 
-$suits = ["clubs", "diamonds", "hearts", "spades"];
+if (isset($_POST["submit"])) {
 
-$randomSuits = array_rand($suits);
+  $number = $_POST["number"];
+  $total = 0;
+  $i = 1;
 
-$cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queem", "King"];
+  $last4 = substr($number, -4, 4);
 
-$randomCard = array_rand($cards);
+  $number = str_split($number);
+
+  $number = array_reverse($number);
+
+  foreach ($number as $digit) {
+
+    if ($i % 2 == 0) {
+      $digit *= 2;
+
+      if ($digit > 9) {
+        $digit -= 9;
+      }
+    }
+
+    $total += $digit;
+
+    $i++;
+  }
+
+  if ($total % 10 == 0) {
+    echo "Your credit card number ending in " . $last4 . " is valid";
+  } else {
+    echo "Your credit card number ending in " . $last4 . " is invalid";
+  }
+}
 
 
-echo "Your card is " . $cards[$randomCard] . " " . $suits[$randomSuits];
+?>
+
+<form action="" method="POST">
+  <input type="text" name="number" value="number">
+  <input type="submit" name="submit" value="submit">
+</form>
