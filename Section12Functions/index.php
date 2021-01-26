43b6d208e90888a54ecb725638bf52f2ec3bc087
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-26 02:03:12 +0000

    ReturnValues

--- Section12Functions/index.php
+++ Section12Functions/index.php
@@ -1,1 +1,15 @@
 <?php
+
+
+function calculate($num1, $num2)
+{
+  $add = $num1 + $num2;
+  $subtract = $num1 - $num2;
+  return [$add, $subtract];
+}
+
+$results = calculate(3, 2);
+
+echo $results[0];
+echo "<br>";
+echo $results[1];
--- Section12Functions/notes.php
+++ Section12Functions/notes.php
@@ -37,3 +37,31 @@ multiply(3, 2);
 echo "<br>";
 
 multiply(5, 5);
+
+
+echo " -----------------------Return SINGLE Value-------------------";
+
+function uppercase($name1)
+{
+
+$result1 = strtoupper($name1);
+return $result1;
+}
+
+echo uppercase("Smith");
+
+
+echo " -------------- Return MULTIPLE Values -------------------";
+
+function calculate($num1, $num2)
+{
+$add = $num1 + $num2;
+$subtract = $num1 - $num2;
+return [$add, $subtract];
+}
+
+$results = calculate(3, 2);
+
+echo $results[0];
+echo "<br>";
+echo $results[1];
