54cbbacdf4b0aae15e050fa34db6b21573d85add
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-03-02 05:39:02 +0000

    practicingDB

---
+++ PHPPDOMySQL/Section1/quick-win.php
@@ -0,0 +1,39 @@
+<?php
+
+// $db_host = 'localhost';
+// $db_name = 'company';
+// $db_username = 'root';
+// $db_password = '';
+
+// $dsn = "mysql:host=$db_host;dbname=$db_name";
+
+// $db_connection = new PDO($dsn, $db_username, $db_password);
+
+// $query = "SELECT * FROM users";
+
+// $results = $db_connection->query($query);
+
+// $db_connection = NULL;
+
+// foreach ($results as $result) {
+//   echo $result['firstName'];
+//   echo " = ";
+//   echo $result['email'];
+//   echo "<br>";
+// }
+
+
+$db_host = 'localhost';
+$db_name = 'company';
+$db_username = 'root';
+$db_password = '';
+
+$dsn = "mysql:host=db_host;name=db_name";
+
+$db_connection = new PDO($dsn, $db_username, $db_password);
+
+$query = "SELECT * FROM users";
+
+$results = $db_connection->query($query);
+
+$db_connection = NULL;
