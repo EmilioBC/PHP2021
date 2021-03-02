cb96b2081fc1e711cb07024b56962ff325c25368
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-03-02 01:02:48 +0000

    ChallengeDarts

--- Section12Functions/index.php
+++ Section12Functions/index.php
@@ -13,3 +13,20 @@ $results = calculate(3, 2);
 echo $results[0];
 echo "<br>";
 echo $results[1];
+
+echo "<br>";
+
+
+function uppercase($first, $last)
+{
+  $firstUpper = strtoupper($first);
+  $lastUpper = strtoupper($last);
+  return [$firstUpper, $lastUpper];
+}
+
+
+$resultName = uppercase("emilio", "comiling");
+
+echo $resultName[0];
+echo " ";
+echo $resultName[1];
